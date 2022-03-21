28b7e90219b740f1b68305c8b257e9c6393a43d0
Michael <michaelaucoin94@gmail.com>
2022-03-21 10:08:38 -0500

    Initial Commit

---
+++ support.py
@@ -0,0 +1,15 @@
+import pygame
+from os import walk
+
+
+def import_folder(path):
+
+    surf_list = []
+
+    for _, __, img_files in walk(path):
+        for img in img_files:
+            full_path = path + "/" + img
+            img_surf = pygame.image.load(full_path).convert_alpha()
+            surf_list.append(img_surf)
+
+    return surf_list
